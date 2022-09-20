9dcf8353e9b324ce04de0cf6b704a606bdaebdae
thisisbramiller <76793954+thisisbramiller@users.noreply.github.com>
2022-09-19 19:37:18 -0500

    feat: :sparkles: add login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  post 'login', to: 'session#destroy'
   
   resources :users, except: [:new] 
   resources :articles
